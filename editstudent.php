ac9432785db7574cc4795edb4ff848c70678918e
Ahwazie <wazie8699@gmail.com>
2023-11-01 01:06:37 +0800

    First ever table for data

---
+++ editstudent.php
@@ -0,0 +1,142 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "wcu";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $id = $_POST['id'];
+    $inisiatif_strategik = $_POST['inisiatif_strategik'];
+    $kpi = $_POST['kpi'];
+    $peneraju = $_POST['peneraju'];
+    $garis_dasar = $_POST['garis_dasar'];
+    $y2024 = $_POST['y2024'];
+    $y2025 = $_POST['y2025'];
+    $y2026 = $_POST['y2026'];
+    $y2027 = $_POST['y2027'];
+    $y2028 = $_POST['y2028'];
+
+    $sql = "UPDATE student SET inisiatif_strategik='$inisiatif_strategik', kpi='$kpi', peneraju='$peneraju', garis_dasar='$garis_dasar', y2024='$y2024', y2025='$y2025', y2026='$y2026', y2027='$y2027', y2028='$y2028' WHERE id=$id";
+
+    if ($conn->query($sql) === TRUE) {
+        header("Location: student.php");
+        exit;
+    } else {
+        echo "Error updating record: " . $conn->error;
+    }
+}
+
+$id = $_GET['id'];
+$sql = "SELECT * FROM student WHERE id=$id";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Student Data</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f7f9fc;
+            margin: 0;
+            padding: 0;
+        }
+
+        h2 {
+            background-color: #2c3e50;
+            color: #ecf0f1;
+            padding: 20px 0;
+            margin: 0;
+        }
+
+        /* Form styling */
+        form {
+            max-width: 600px;
+            margin: 50px auto;
+            padding: 20px;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        label {
+            display: block;
+            margin-bottom: 8px;
+            font-weight: bold;
+        }
+
+        input[type="text"] {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 10px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            font-size: 16px;
+        }
+
+        input[type="submit"] {
+            padding: 10px 15px;
+            background-color: #1E90FF;
+            color: #fff;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+            font-size: 16px;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #3498db;
+        }
+    </style>
+</head>
+<body>
+
+<h2 style="text-align:center;">Edit Data</h2>
+
+<form action="editstudent.php" method="post">
+    <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+    <label for="inisiatif_strategik">INISIATIF STRATEGIK:</label>
+    <input type="text" name="inisiatif_strategik" value="<?php echo $row['inisiatif_strategik']; ?>">
+    
+    <label for="kpi">KPI:</label>
+    <input type="text" name="kpi" value="<?php echo $row['kpi']; ?>">
+    
+    <label for="peneraju">PENERAJU:</label>
+    <input type="text" name="peneraju" value="<?php echo $row['peneraju']; ?>">
+    
+    <label for="garis_dasar">GARIS DASAR:</label>
+    <input type="text" name="garis_dasar" value="<?php echo $row['garis_dasar']; ?>">
+    
+    <label for="y2024">2024:</label>
+    <input type="text" name="y2024" value="<?php echo $row['y2024']; ?>">
+    
+    <label for="y2025">2025:</label>
+    <input type="text" name="y2025" value="<?php echo $row['y2025']; ?>">
+    
+    <label for="y2026">2026:</label>
+    <input type="text" name="y2026" value="<?php echo $row['y2026']; ?>">
+    
+    <label for="y2027">2027:</label>
+    <input type="text" name="y2027" value="<?php echo $row['y2027']; ?>">
+    
+    <label for="y2028">2028:</label>
+    <input type="text" name="y2028" value="<?php echo $row['y2028']; ?>">
+    
+    <input type="submit" value="Update">
+</form>
+
+</body>
+</html>
+
+<?php
+$conn->close();
+?>
--- student.php
+++ student.php
@@ -1,6 +1,24 @@
 <?php
-// Start the output buffer
-ob_start();
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "wcu";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM student";
+$result = $conn->query($sql);
+
+function determineColor($value) {
+    if (substr($value, -1) == "%" && intval(rtrim($value, "%")) < 70) {
+        return "color: red;";
+    }
+    return "";
+}
 ?>
 
 <!DOCTYPE html>
@@ -8,29 +26,61 @@ ob_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Teras Strategik 1</title>
+    <title>Student Quality</title>
     <style>
-        /* Styles for the table */
+        body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f7f9fc;
+            margin: 0;
+            padding: 0;
+        }
+
+        h2 {
+            background-color: #2c3e50;
+            color: #ecf0f1;
+            padding: 20px 0;
+            margin: 0;
+        }
+
         table {
             border-collapse: collapse;
-            margin: 0 auto; /* center the table */
-            width: 80%; /* table width */
+            margin: 40px auto;
+            width: 90%;
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
         }
-        table, th, td {
-            border: 1px solid black;
-        }
+
         th, td {
-            padding: 10px;
-            text-align: left;
+            padding: 12px 15px;
+            border-bottom: 1px solid #ddd;
         }
+
         th {
-            background-color: #1E90FF;
+            background-color: #34495e;
             color: white;
+            text-transform: uppercase;
+            font-weight: normal;
+        }
+
+        td {
+            background-color: #f7f9fc;
+        }
+
+        tr:hover td {
+            background-color: #dfe7f1;
+        }
+
+        a {
+            text-decoration: none;
+            color: #3498db;
+        }
+
+        a:hover {
+            text-decoration: underline;
         }
     </style>
 </head>
 <body>
-    <h2 style="text-align:center;">Teras Strategik 1</h2>
+    <h2 style="text-align:center;">Quality Student</h2>
 
     <table>
         <thead>
@@ -44,58 +94,64 @@ ob_start();
                 <th>2026</th>
                 <th>2027</th>
                 <th>2028</th>
+                <th>ACTION</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td rowspan="4">1.1<br>Menyediakan tenaga akademik yang berkualiti tinggi dan kompeten</td>
-                <td>Nisbah tenaga akademik dan pelajar</td>
-                <td>Pendaftar & PR Akademik (HR, PPA & PPS)</td>
-                <td>1:13</td>
-                <td>1:13</td>
-                <td>1:13</td>
-                <td>1:12</td>
-                <td>1:11</td>
-                <td>1:10</td>
-            </tr>
-            <tr>
-                <td>% tenaga akademik yang mempunyai kelulusan PhD</td>
-                <td>Pendaftar (HR)</td>
-                <td>70%</td>
-                <td>71%</td>
-                <td>72%</td>
-                <td>73%</td>
-                <td>74%</td>
-                <td>75%</td>
-            </tr>
-            <tr>
-                <td>% tenaga akademik yang mengikuti latihan pembangunan profesional</td>
-                <td>Pendaftar & PR Akademik (HR & C4L)</td>
-                <td>75%</td>
-                <td>80%</td>
-                <td>85%</td>
-                <td>90%</td>
-                <td>95%</td>
-                <td>100%</td>
-            </tr>
-            <tr>
-                <td>% tahap kepuasan pelajar terhadap pengajaran dan pembelajaran</td>
-                <td>PR PAP (PPPS)</td>
-                <td>75%</td>
-                <td>76%</td>
-                <td>77%</td>
-                <td>78%</td>
-                <td>79%</td>
-                <td>80%</td>
-            </tr>
+            <?php
+            if ($result->num_rows > 0) {
+                $firstRow = true;
+                while($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    if ($firstRow) {
+                        echo "<td rowspan='4' style='vertical-align:middle; text-align:center;'>" . $row["inisiatif_strategik"] . "</td>";
+                        $firstRow = false;
+                    }
+                    echo "<td>" . $row["kpi"] . "</td>";
+                    echo "<td>" . $row["peneraju"] . "</td>";
+                    echo "<td style='" . determineColor($row["garis_dasar"]) . "'>" . $row["garis_dasar"] . "</td>";
+                    echo "<td style='" . determineColor($row["y2024"]) . "'>" . $row["y2024"] . "</td>";
+                    echo "<td style='" . determineColor($row["y2025"]) . "'>" . $row["y2025"] . "</td>";
+                    echo "<td style='" . determineColor($row["y2026"]) . "'>" . $row["y2026"] . "</td>";
+                    echo "<td style='" . determineColor($row["y2027"]) . "'>" . $row["y2027"] . "</td>";
+                    echo "<td style='" . determineColor($row["y2028"]) . "'>" . $row["y2028"] . "</td>";
+                    echo "<td><a href='editstudent.php?id=" . $row["id"] . "'><img src='icons/edit.png' alt='Edit' style='width:30px; height:auto;'></a></td>";
+
+                    echo "</tr>";
+                }
+            } else {
+                echo "<tr><td colspan='10'>No data found</td></tr>";
+            }
+            ?>
         </tbody>
     </table>
+
+    <?php
+    $conn->close();
+    ?>
 
 </body>
 </html>
 
-<?php
-// End the output buffer and send the HTML to the browser
-echo ob_get_clean();
-?>
 
+<!-- SQL queries nya
+
+CREATE TABLE student (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    inisiatif_strategik VARCHAR(255),
+    kpi VARCHAR(255),
+    peneraju VARCHAR(255),
+    garis_dasar VARCHAR(50),
+    y2024 VARCHAR(50),
+    y2025 VARCHAR(50),
+    y2026 VARCHAR(50),
+    y2027 VARCHAR(50),
+    y2028 VARCHAR(50)
+);
+
+random data bwh ani:
+INSERT INTO student (inisiatif_strategik, kpi, peneraju, garis_dasar, y2024, y2025, y2026, y2027, y2028) VALUES
+('1.1 Menyediakan tenaga akademik yang berkualiti tinggi dan kompeten', 'Nisbah tenaga akademik dan pelajar', 'Pendaftar & PR Akademik (HR, PPA & PPS)', '1:13', '1:13', '1:13', '1:12', '1:11', '1:10'),
+('1.1 Menyediakan tenaga akademik yang berkualiti tinggi dan kompeten', '% tenaga akademik yang mempunyai kelulusan PhD', 'Pendaftar (HR)', '70%', '71%', '72%', '73%', '74%', '75%'),
+('1.1 Menyediakan tenaga akademik yang berkualiti tinggi dan kompeten', '% tenaga akademik yang mengikuti latihan pembangunan profesional', 'Pendaftar & PR Akademik (HR & C4L)', '75%', '80%', '85%', '90%', '95%', '100%'),
+('1.1 Menyediakan tenaga akademik yang berkualiti tinggi dan kompeten', '% tahap kepuasan pelajar terhadap pengajaran dan pembelajaran', 'PR PAP (PPPS)', '75%', '76%', '77%', '78%', '79%', '80%');
